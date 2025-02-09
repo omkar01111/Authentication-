2b44f8ca5909f8ba7649dfb4de3fdbc9b2ac5709
omkar <omkarmore0702@gmail.com>
2025-02-09 17:32:58 +0530

    CLient url changes

--- Backend/passportJS/passport.js
+++ Backend/passportJS/passport.js
@@ -14,7 +14,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback",
+      callbackURL: `${process.env.CLIENT_URL}/api/auth/google/callback`
+ ||" http://localhost:5000/api/auth/google/callback",
+     
+      
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
